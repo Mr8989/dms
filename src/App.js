e08ad9e88ecac1bc78009980dbcccd5d43a1652c
dickson anyaele <dicksonanyaele1234@gmail.com>
2023-08-15 03:11:39 -1200

    add react router and page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './login/Login';
+import Course from './CoursePage/Course';
+import Home from './HomePage/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<BrowserRouter>
+        <Routes>
+            <Route path="/login"
+                element={<Login/>}/>
+
+            <Route path='/course/:id'
+                element={<Course/>}/>
+            <Route path="/"
+                element={<Home/>}/>
+        </Routes>
+    </BrowserRouter>);
 }
 
 export default App;
